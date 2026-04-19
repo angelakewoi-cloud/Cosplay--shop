aa3d165417ff40a9cc5702bf502feff5f1ac9693
angelakewoi-cloud <angelakewoi@gmail.com>
2026-04-19 16:31:35 +0100

    Implement cart functionality with add and display methods

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+let cart=[];
 
+function addToCart(name,price){
+  cart.push({name,price});
+  alert(name+" ajouté au panier !");
+}
+
+function displayCart(){
+  let list=document.getElementById("cartItems");
+  let total=0;
+  cart.forEach(item=>{
+    list.innerHTML+=`<p>${item.name} - $${item.price}</p>`;
+    total+=item.price;
+  });
+  document.getElementById("total").innerText="$"+total;
+}
